202a07a9ed3e805204aacd1cb169e10e18c4037a
Andrii Ahapov <andrii.ahapov@sap.com>
2018-05-07 12:56:24 +0200

    Added local gitlab service instance

--- infrabox/utils/gitlab/run_gitlab.sh
+++ infrabox/utils/gitlab/run_gitlab.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+#docker-compose build --no-cache
+docker-compose up
+#sudo docker run --detach \
+#    --hostname gitlab.infrabox.com \
+#    --publish 30080:80 --publish 30022:22 \
+#    --name gitlab \
+#    gitlab/gitlab-ce
